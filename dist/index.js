0689e356102ae4286bad16e53b70bb2d2bded30f
Suganth <suganth.s1987@gmail.com>
2021-09-03 13:12:17 -0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,3 @@
-module.exports = ({github, context}) => {
-	
-	if (['dev', 'test', 'prod'].includes(context.process.env.environment)) {
-	  console.log("Deployment environment is validated successfully to proceed with workflow");
-	}
-
-	else {
-		console.log("Deployment environment must be either 'dev', 'test' or 'prod'");
-		process.exit(1);
-	}
-}
+const nameToGreet = core.getInput('who-to-greet');
+console.log(`Hello ${nameToGreet}!`);
 
