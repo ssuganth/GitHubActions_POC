bc9f172fb1fb8d4025d6c002da8d4261d9e76238
Suganth <suganth.s1987@gmail.com>
2021-09-03 12:36:08 -0700

    update

--- .github/scripts/validateEnv.js
+++ .github/scripts/validateEnv.js
@@ -1,8 +1,12 @@
-if (['dev', 'test', 'prod'].includes(process.env.environment)) {
-  console.log("Deployment environment is validated successfully to proceed with workflow");
+module.exports = ({github, context}) => {
+	
+	if (['dev', 'test', 'prod'].includes(context.process.env.environment)) {
+	  console.log("Deployment environment is validated successfully to proceed with workflow");
+	}
+
+	else {
+		console.log("Deployment environment must be either 'dev', 'test' or 'prod'");
+		process.exit(1);
+	}
 }
 
-else {
-	console.log("Deployment environment must be either 'dev', 'test' or 'prod'");
-	process.exit(1);
-}
